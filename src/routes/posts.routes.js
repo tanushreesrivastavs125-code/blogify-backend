65c759555daa549917a74ad2b8f0c9ea53b5d37e
tanushreesrivastavs125-code <tanushree.srivastav.s.125@kalvium.community>
2026-02-07 11:09:43 +0530

    hello!

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Fetching all blog posts...');
-});
+// Import controller
+const postController = require('../controllers/posts.controller');
+
+// Route → Controller
+router.get('/', postController.getAllPosts);
 
 module.exports = router;
